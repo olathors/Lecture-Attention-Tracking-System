40d546d18b5947bda1f13fff4170e563c0684c31
hakmoe <haakon.oyehaug.moe@gmail.com>
2024-05-20 16:01:43 +0200

    got video player setup working

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Lecture-Attention-Tracking-System
 Project in the course TEK5030 at UiO
+
+
+## main-file
--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from distracted_function import distracted
 import cv2
 from gaze_tracking import GazeTracking
 import time
+import subprocess
+commands = {
+    "start":"cvlc",
+    "pause":"dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2   org.mpris.MediaPlayer2.Player.PlayPause",
+    "play":"dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2   org.mpris.MediaPlayer2.Player.Play",
+    "stop":"dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2   org.mpris.MediaPlayer2.Player.Stop"
+}
+
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture(0)
@@ -10,6 +18,7 @@ if __name__ == "__main__":
     gaze = GazeTracking()
     counter = 0
     sec_per_loop = 0.1
+    process = subprocess.Popen(commands["start"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     while True:
         while counter < 50:
             # 1. Setup the timing: https://corporatefinanceinstitute.com/resources/career-map/sell-side/capital-markets/exponentially-weighted-moving-average-ewma/
@@ -19,7 +28,8 @@ if __name__ == "__main__":
             null, frame = cap.read()
             # True when the person is sleepy
             drowsy_bool = drowsy(frame=frame)
-            distracted_bool = distracted(frame=frame, gaze=gaze)
+            # distracted_bool = distracted(frame=frame, gaze=gaze)
+            distracted_bool = False
             if drowsy_bool or distracted_bool:
                 counter += 1
             elif counter > 0:
@@ -30,7 +40,13 @@ if __name__ == "__main__":
             if remaining_time > 0:
                 time.sleep(remaining_time)
 
-        print("Follow the video you lazy")        
+        process = subprocess.Popen(commands["pause"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # pauses the video
+        # starts the code for the handpose estimator 
+        time.sleep(5)
         counter = 0
+        # starts the video again
+        process = subprocess.Popen(commands["play"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
 
         
